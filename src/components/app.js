4d365e802301de32b6b77526521e0b1b1373edd5
lemonchip <Hekaichuan@outlook.com>
2019-08-29 19:00:58 +0800

    初始化

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import Button from 'antd/es/button';
+import { Button } from 'antd-mobile';
+import './app.less'
+//import './App.css';
+
+  class App extends Component {
+    render() {
+      return (
+        <div>
+          <Button type="primary">Button</Button>
+        </div>
+      );
+    }
+  }
+
+  export default App;
