ec099979d1a9e64880dfbb8545ddc468ae1d2227
lemonchip <Hekaichuan@outlook.com>
2019-09-20 19:02:43 +0800

    完善组件

--- src/containers/personalTop/personalTop.jsx
+++ src/containers/personalTop/personalTop.jsx
@@ -18,10 +18,10 @@ export default class PersonalTop  extends Component {
                 <div>粉&nbsp;&nbsp;丝<span>8888</span></div>
                 <div>帖&nbsp;&nbsp;子<span>8888</span></div>
                 <div>获&nbsp;&nbsp;赞<span>8888</span></div>
-            {/* <span>关注</span>
-            <span>粉丝</span>
-            <span>帖子</span>
-            <span>获赞</span> */}
+                <span>关注</span>
+                <span>粉丝</span>
+                <span>帖子</span>
+                <span>获赞</span>
             </div>
           
             </div>
